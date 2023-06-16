251459b8c7eacb56e517e3cb7870614104d0c026
Amos Barbato <amosbarbato@gmail.com>
2023-06-16 17:25:51 -0300

    collapse day 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,19 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
-function styles () {
+function styles() {
     return gulp.src('./src/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist'));
 }
 
-function images () {
-    return gulp.src('./lib/img/**/*')
+function images() {
+    return gulp.src('./assets/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/img'));
 }
 
 exports.default = gulp.parallel(styles, images);
-
 
 exports.watch = function() {
     gulp.watch('./src/*.scss', gulp.parallel(styles))
